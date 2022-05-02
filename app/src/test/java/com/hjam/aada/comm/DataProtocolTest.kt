e4619e238c919041b2a4d670c0c34856a1e57e0c
H1Jam <hosseinjamshidien@gmail.com>
2022-05-02 02:23:37 -0400

    Update DataProtocolTest.kt

--- app/src/test/java/com/hjam/aada/comm/DataProtocolTest.kt
+++ app/src/test/java/com/hjam/aada/comm/DataProtocolTest.kt
@@ -1,62 +1,65 @@
 package com.hjam.aada.comm
 
-import com.hjam.aada.MainActivity
 import junit.framework.TestCase
 
+//Todo: Assert Null on each that must be null and non-null on the one that shouldn't be null.
 class DataProtocolTest : TestCase() {
 
     fun testParseIt() {
-        println(DataProtocol.parseIt(10))
-        println(DataProtocol.parseIt(12))
-        println(DataProtocol.parseIt(13))
-        println(DataProtocol.parseIt(14))
-        println(DataProtocol.parseIt(16))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(3))
-        println(DataProtocol.parseIt(6))
-        println(DataProtocol.parseIt(7))
-        println(DataProtocol.parseIt(8))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(1))
-        println(DataProtocol.parseIt(128))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(0))
-        println(DataProtocol.parseIt(21))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(2))
-        println(DataProtocol.parseIt(33))
-        println(DataProtocol.parseIt(33))
+        assertNull(DataProtocol.parseIt(10))
+        assertNull(DataProtocol.parseIt(12))
+        assertNull(DataProtocol.parseIt(13))
+        assertNull(DataProtocol.parseIt(14))
+        assertNull(DataProtocol.parseIt(16))
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(3))
+        assertNull(DataProtocol.parseIt(6))
+        assertNull(DataProtocol.parseIt(7))
+        assertNotNull(DataProtocol.parseIt(8))
 
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(3))
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(1))
-        println(DataProtocol.parseIt(33))
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(1))
+        assertNotNull(DataProtocol.parseIt(128))
 
-        println(DataProtocol.parseIt(199))
-        println(DataProtocol.parseIt(201))
-        println(DataProtocol.parseIt(176))
-        println(DataProtocol.parseIt(256))
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(0))
+        assertNull(DataProtocol.parseIt(21))
+
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(2))
+        assertNull(DataProtocol.parseIt(33))
+        val a2 = DataProtocol.parseIt(33)
+        assertNotNull(a2)
+        println(a2)
+
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(3))
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNotNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(1))
+        assertNull(DataProtocol.parseIt(33))
+
+        assertNull(DataProtocol.parseIt(199))
+        assertNull(DataProtocol.parseIt(201))
+        assertNull(DataProtocol.parseIt(176))
+        assertNull(DataProtocol.parseIt(256))
         for (i in 0..255) {
             val a = DataProtocol.parseIt(i)
             if (a != null) {
                 println(a)
             }
         }
-
-
     }
 }
