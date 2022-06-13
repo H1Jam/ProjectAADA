b0eea31a55c7c7f5538e899dd273346c9b3ec27a
H1Jam <hosseinjamshidien@gmail.com>
2022-06-13 02:15:23 -0400

    Update crc16Test.kt

--- app/src/test/java/com/hjam/aada/utils/crc16Test.kt
+++ app/src/test/java/com/hjam/aada/utils/crc16Test.kt
@@ -9,7 +9,7 @@ class Crc16Test : TestCase() {
     fun test_CRC_to_array() {
         val inp = byteArrayOf(49,50,51,52,53,54,55,56,57)
         val crcActual = Crc16.crc16(inp)
-        val expected = byteArrayOf(0xE5.toByte(), 0xCC.toByte())
+        val expected = byteArrayOf(0xCC.toByte(), 0xE5.toByte())
         assertTrue(expected.contentEquals(crcActual))
     }
 
