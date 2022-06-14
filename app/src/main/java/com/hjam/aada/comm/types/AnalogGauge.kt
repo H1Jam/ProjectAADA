aeb984a639fc96ac13b5a79146a06d66f9a60166
H1Jam <hosseinjamshidien@gmail.com>
2022-06-14 01:35:12 -0400

    Create AnalogGauge.kt

---
+++ app/src/main/java/com/hjam/aada/comm/types/AnalogGauge.kt
@@ -0,0 +1,13 @@
+package com.hjam.aada.comm.types
+
+class AnalogGauge(
+    private val x: Int,
+    private val y: Int,
+    private val width: Int,
+    private val height: Int
+) {
+    public fun set() {
+
+    }
+
+}
