53be812069adc32a75e9ceaf450eb9ff4c8ba505
H1Jam <hosseinjamshidien@gmail.com>
2022-07-09 21:45:12 -0400

    Update AADAKnobTest.kt

--- app/src/test/java/com/hjam/aada/comm/types/AADAKnobTest.kt
+++ app/src/test/java/com/hjam/aada/comm/types/AADAKnobTest.kt
@@ -4,7 +4,7 @@ import junit.framework.TestCase
 
 class AADAKnobTest : TestCase(){
     fun test_screenTag(){
-       val a = AADAKnob(10,20,30,200,0,0,43)
+       val a = AADAKnob(10,20,30,200,0,0,"TxT",43)
         assertEquals("knb43",a.screenTag)
     }
 }
