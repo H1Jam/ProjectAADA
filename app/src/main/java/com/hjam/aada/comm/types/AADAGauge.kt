ffc8f26b59882be1fb5ae40ce18a0438a431645b
H1Jam <hosseinjamshidien@gmail.com>
2022-07-14 21:04:23 -0400

    Update AADAGauge.kt

--- app/src/main/java/com/hjam/aada/comm/types/AADAGauge.kt
+++ app/src/main/java/com/hjam/aada/comm/types/AADAGauge.kt
@@ -1,19 +1,55 @@
 package com.hjam.aada.comm.types
 
-class AADAGauge(
-    private var tag: String,
-    private val x: Int,
-    private val y: Int,
-    private val width: Int,
-    private val height: Int,
-    private var value: Float
-) {
-    companion object{
-        val objID : Byte = AADAObject.ScreenIDs.gauge1.ordinal.toByte()
-        val typeArrayList = arrayOf(Long, Float, Int)
-    }
-    public fun setX() {
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.charset.StandardCharsets
 
+class AADAGauge(
+    var x: Int,
+    var y: Int,
+    var size: Int,
+    val maxValue: Float,
+    val drawArc: Boolean,
+    var arcGreenMaxVal: Float,
+    var arcYellowMaxVal: Float,
+    var arcRedMaxVal: Float,
+    val unitTextLabel: String,
+    val tag: Int
+) : AADAObject(tag, "gau") {
+    companion object {
+        val objID = ScreenIDs.gauge1.ordinal.toByte()
+        fun fromByteBuffer(byteBuffer: ByteBuffer): AADAGauge {
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+            val x = byteBuffer.short.toInt()
+            val y = byteBuffer.short.toInt()
+            val cTag = byteBuffer.short.toInt()
+            val size = byteBuffer.short.toInt()
+            val maxValue = byteBuffer.float
+            val drawArc = byteBuffer.char != 0.toChar()
+            val arcGreenMaxVal = byteBuffer.float
+            val arcYellowMaxVal = byteBuffer.float
+            val arcRedMaxVal = byteBuffer.float
+            val unitTextLabel = StandardCharsets.UTF_8.decode(byteBuffer).toString()
+            return AADAGauge(
+                x,
+                y,
+                size,
+                maxValue,
+                drawArc,
+                arcGreenMaxVal,
+                arcYellowMaxVal,
+                arcRedMaxVal,
+                unitTextLabel,
+                cTag
+            )
+        }
+    }
+
+    override fun toString(): String {
+        return "AADAGauge:[Tag:$tag (${screenTag}), x:$x, y:$y, Size:$size maxValue:$maxValue," +
+                " drawArc:$drawArc, arcGreenMaxVal:$arcGreenMaxVal, " +
+                "arcYellowMaxVal:$arcYellowMaxVal, arcRedMaxVal:$arcRedMaxVal, " +
+                "unitTextLabel:$unitTextLabel]"
     }
 
 }
