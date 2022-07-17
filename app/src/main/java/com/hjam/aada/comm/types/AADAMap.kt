e50f1e1c772b1dca0c08d0cedc5c325f9841da80
H1Jam <hosseinjamshidien@gmail.com>
2022-07-17 19:29:00 -0400

    Fix AADAMap companion objects

--- app/src/main/java/com/hjam/aada/comm/types/AADAMap.kt
+++ app/src/main/java/com/hjam/aada/comm/types/AADAMap.kt
@@ -14,28 +14,29 @@ class AADAMap(
     var zoom: Float,
     val tag: Int = 1 // Always 1, since we only have 1 map on screen (may extend it in the future).
 ) : AADAObject(tag, "map") {
-    val objID = ScreenIDs.Map.ordinal.toByte()
-    fun fromByteBuffer(byteBuffer: ByteBuffer): AADAMap {
-        byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
-        val x = byteBuffer.short.toInt()
-        val y = byteBuffer.short.toInt()
-        val cTag = byteBuffer.short.toInt()// Reserved
-        val height = byteBuffer.short.toInt()
-        val width = byteBuffer.short.toInt()
-        val lat = byteBuffer.float
-        val lon = byteBuffer.float
-        val zoom = byteBuffer.float
-        return AADAMap(
-            x,
-            y,
-            height,
-            width,
-            lat,
-            lon,
-            zoom
-        )
+    companion object {
+        val objID = ScreenIDs.Map.ordinal.toByte()
+        fun fromByteBuffer(byteBuffer: ByteBuffer): AADAMap {
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+            val x = byteBuffer.short.toInt()
+            val y = byteBuffer.short.toInt()
+            val cTag = byteBuffer.short.toInt()// Reserved
+            val height = byteBuffer.short.toInt()
+            val width = byteBuffer.short.toInt()
+            val lat = byteBuffer.float
+            val lon = byteBuffer.float
+            val zoom = byteBuffer.float
+            return AADAMap(
+                x,
+                y,
+                height,
+                width,
+                lat,
+                lon,
+                zoom
+            )
+        }
     }
-
     override fun toString(): String {
         return "AADAMap:[Tag:$tag (${screenTag}), x:$x, y:$y, height:$height, width:$width, " +
                 "lat:$lat, lon:$lon, zoom:$zoom]"
