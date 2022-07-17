e3ca9fd10e7b549a25fc28e116f8ee19fcd368cb
H1Jam <hosseinjamshidien@gmail.com>
2022-07-17 18:53:16 -0400

    add AADAMap fromByteBuffer()

--- app/src/main/java/com/hjam/aada/comm/types/AADAMap.kt
+++ app/src/main/java/com/hjam/aada/comm/types/AADAMap.kt
@@ -1,5 +1,9 @@
 package com.hjam.aada.comm.types
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.charset.StandardCharsets
+
 class AADAMap(
     val x: Int,
     val y: Int,
@@ -8,7 +12,21 @@ class AADAMap(
     val tag: Int
 ) : AADAObject(tag, "map")  {
     val objID = ScreenIDs.Map.ordinal.toByte()
-
+    fun fromByteBuffer(byteBuffer: ByteBuffer): AADAMap {
+        byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+        val x = byteBuffer.short.toInt()
+        val y = byteBuffer.short.toInt()
+        val cTag = byteBuffer.short.toInt()
+        val height = byteBuffer.short.toInt()
+        val width = byteBuffer.short.toInt()
+        return AADAMap(
+            x,
+            y,
+            height,
+            width,
+            cTag
+        )
+    }
 
     override fun toString(): String {
         return "AADAMap:[Tag:$tag (${screenTag}), x:$x, y:$y, height:$height, width:$width]"
