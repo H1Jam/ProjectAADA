599eb623d784435a7464b964bf9ed715962410bb
H1Jam <hosseinjamshidien@gmail.com>
2022-07-19 22:12:35 -0400

    pass the Context to DataStateMachine

--- app/src/main/java/com/hjam/aada/comm/types/DataStateMachine.kt
+++ app/src/main/java/com/hjam/aada/comm/types/DataStateMachine.kt
@@ -1,5 +1,6 @@
 package com.hjam.aada.comm.types
 
+import android.content.Context
 import com.hjam.aada.ScreenObjects
 import com.hjam.aada.utils.Logger
 import java.nio.ByteBuffer
@@ -8,7 +9,7 @@ class DataStateMachine {
     companion object{
         private const val mTag = "DataStateMachine"
         var switch :Byte = 0
-        fun process(bb: ByteBuffer){
+        fun process(bb: ByteBuffer, context: Context){
             Logger.debug(mTag,"DataStateMachine.process!")
             switch = bb.get()
             Logger.debug(mTag,"DataStateMachine.process! mID:$switch")
@@ -16,32 +17,37 @@ class DataStateMachine {
                 AADATextLabel.objID->{
                     val textLabel = AADATextLabel.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a TextLabel $textLabel")
-                    ScreenObjects.addTextLabel(textLabel)
+                    ScreenObjects.addTextLabel(textLabel, context)
                 }
                 AADAButton.objID->{
                     val aadaButton = AADAButton.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a AADAButton $aadaButton")
-                    ScreenObjects.addButton(aadaButton)
+                    ScreenObjects.addButton(aadaButton, context)
                 }
                 AADAKnob.objID->{
                     val aadaKnob = AADAKnob.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a AADAKnob $aadaKnob")
-                    ScreenObjects.addKnob(aadaKnob)
+                    ScreenObjects.addKnob(aadaKnob, context)
                 }
                 AADAGauge.objID->{
                     val aadaGauge = AADAGauge.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a AADAGauge $aadaGauge")
-                    ScreenObjects.addGauge(aadaGauge)
+                    ScreenObjects.addGauge(aadaGauge, context)
                 }
                 AADAMap.objID->{
                     val aadaMap = AADAMap.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a AADAMap $aadaMap")
-                    ScreenObjects.addMap(aadaMap)
+                    ScreenObjects.addMap(aadaMap, context)
                 }
                 AADAMapMarker.objID->{
                     val aadaMapMarker = AADAMapMarker.fromByteBuffer(bb)
                     Logger.debug(mTag, "Got a AADAMapMarker $aadaMapMarker")
                     ScreenObjects.addMapMarker(aadaMapMarker)
+                }
+                AADASwitch.objID->{
+                    val aadaSwitch = AADASwitch.fromByteBuffer(bb)
+                    Logger.debug(mTag, "Got a aadaSwitch $aadaSwitch")
+                    ScreenObjects.addSwitch(aadaSwitch, context)
                 }
                 else ->{
                     Logger.debug(mTag, "Got a unknown command ID!")
