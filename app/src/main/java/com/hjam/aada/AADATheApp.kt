0ae74ea59c5f38e10c74ab728011eba40e6fbbd7
H1Jam <hosseinjamshidien@gmail.com>
2022-07-19 22:14:10 -0400

    Update AADATheApp.kt

--- app/src/main/java/com/hjam/aada/AADATheApp.kt
+++ app/src/main/java/com/hjam/aada/AADATheApp.kt
@@ -2,15 +2,14 @@ package com.hjam.aada
 
 import android.app.Application
 
-class AADATheApp: Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-    }
-
-    companion object {
-        lateinit var instance: AADATheApp
-            private set
-    }
-}
+//class AADATheApp: Application() {
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//    }
+////
+////    companion object {
+////        lateinit var instance: AADATheApp
+////            private set
+////    }
+//}
