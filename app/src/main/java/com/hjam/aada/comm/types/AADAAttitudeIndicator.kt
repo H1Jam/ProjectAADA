0636f0f8b179d2a5e60e3e60c854c3ee177f9de9
H1Jam <hosseinjamshidien@gmail.com>
2022-07-24 01:17:39 -0400

    expand AADAAttitudeIndicator

--- app/src/main/java/com/hjam/aada/comm/types/AADAAttitudeIndicator.kt
+++ app/src/main/java/com/hjam/aada/comm/types/AADAAttitudeIndicator.kt
@@ -1,5 +1,9 @@
 package com.hjam.aada.comm.types
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.charset.StandardCharsets
+
 class AADAAttitudeIndicator(
     var x: Int,
     var y: Int,
@@ -8,9 +12,32 @@ class AADAAttitudeIndicator(
     val size: Int,
     var roll: Float,
     var pitch: Float
-): AADAObject(tag, "att") {
+) : AADAObject(tag, "att") {
     companion object {
         val objID = ScreenIDs.AttitudeIndicator.ordinal.toByte()
+        fun fromByteBuffer(byteBuffer: ByteBuffer): AADAAttitudeIndicator {
+            byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
+            val x = byteBuffer.short.toInt()
+            val y = byteBuffer.short.toInt()
+            val cTag = byteBuffer.short.toInt()
+            val size = byteBuffer.short.toInt()
+            val cmdId = byteBuffer.get().toInt()
+            val roll = byteBuffer.float
+            val pitch = byteBuffer.float
+            return AADAAttitudeIndicator(
+                x,
+                y,
+                cTag,
+                cmdId,
+                size,
+                roll,
+                pitch
+            )
+        }
     }
 
+    override fun toString(): String {
+        return "AADAGauge:[Tag:$tag (${screenTag}), x:$x, y:$y, cmdId:$cmdId Size:$size, " +
+                "roll:$roll, pitch:$pitch]"
+    }
 }
